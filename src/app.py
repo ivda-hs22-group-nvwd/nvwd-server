5d519f248c31b735fbd9d1a92dc584e3b6f267d7
Nicolas@Ares <huber.nicolas2@uzh.ch>
2022-11-25 11:27:54 +0100

    poc server normalization

--- src/app.py
+++ src/app.py
@@ -2,6 +2,55 @@ import sqlite3
 import json
 from flask import Flask, render_template, request
 
+### QUICK TEST WILL BE REFACTORED
+import pandas as pd
+import numpy as np
+from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
+
+continuous_cols = ['age', 'height']
+categorical_cols = ['body_type', 'drinks', 'drugs', 'income', 'job', 'orientation', 'sex', 'smokes',
+'diet','diet_modifier',
+'education_status', 'education_institution',
+'offspring_status', 'offspring_future',
+'pets_cats', 'pets_dogs',
+'religion_type', 'religion_modifier',
+'sign', 'sign_modifier']
+ethnities_cols = ['ethnicities_middle_eastern', 'ethnicities_hispanic_/_latin',
+       'ethnicities_white', 'ethnicities_indian', 'ethnicities_other',
+       'ethnicities_asian', 'ethnicities_black', 'ethnicities_native_american',
+       'ethnicities_pacific_islander']
+speaks_cols = ['speaks_english', 'speaks_spanish', 'speaks_french', 'speaks_c++',
+       'speaks_chinese', 'speaks_tagalog', 'speaks_portuguese',
+       'speaks_japanese', 'speaks_russian', 'speaks_ukrainian',
+       'speaks_sanskrit', 'speaks_thai', 'speaks_hindi', 'speaks_sign',
+       'speaks_swedish', 'speaks_german', 'speaks_italian', 'speaks_arabic',
+       'speaks_latin', 'speaks_other', 'speaks_hebrew', 'speaks_hawaiian',
+       'speaks_korean', 'speaks_ancient', 'speaks_vietnamese',
+       'speaks_indonesian', 'speaks_latvian', 'speaks_hungarian',
+       'speaks_lisp', 'speaks_swahili', 'speaks_rotuman', 'speaks_czech',
+       'speaks_yiddish', 'speaks_greek', 'speaks_catalan', 'speaks_croatian',
+       'speaks_farsi', 'speaks_icelandic', 'speaks_tamil', 'speaks_serbian',
+       'speaks_esperanto', 'speaks_norwegian', 'speaks_bengali',
+       'speaks_dutch', 'speaks_urdu', 'speaks_irish', 'speaks_welsh',
+       'speaks_sign_language', 'speaks_khmer', 'speaks_cebuano',
+       'speaks_afrikaans', 'speaks_albanian', 'speaks_romanian',
+       'speaks_polish', 'speaks_turkish', 'speaks_finnish']
+mapper = DataFrameMapper(
+  [([continuous_col], StandardScaler()) for continuous_col in continuous_cols] +
+  [(categorical_col, LabelEncoder()) for categorical_col in categorical_cols] +
+  [(ethnities_col, LabelEncoder()) for ethnities_col in ethnities_cols] +
+  [(speaks_col, LabelEncoder()) for speaks_col in speaks_cols],
+  df_out=True 
+)
+
+df_clean = pd.read_csv('../pipeline/data/cleaned.csv')
+sample = df_clean.iloc[:1]
+df_std = np.round(mapper.fit_transform(df_clean.copy()),2)
+
+
 # Start with 'flask --debug run'
 
 ### INIT
@@ -81,3 +130,19 @@ def row(id):
         std = cursor.fetchall()
         
         return json.dumps(clean+std)
+
+
+## DEV-LOGIC
+@app.route('/api/dev',methods = ['POST'])
+def user_input():
+    if request.method == 'POST':
+        
+        
+        # TODO: THis mocks fake input
+        result_orig = sample.to_json(orient="split")
+        parsed_orig = json.loads(result_orig) 
+        
+        sample_std = np.round(mapper.transform(sample), 2) # standardize
+        result_std = sample_std.to_json(orient="split") 
+        parsed_std = json.loads(result_std)
+        return json.dumps(parsed_std) # TODO: print both
